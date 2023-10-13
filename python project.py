030660458c1194b24cc3b20f79525323b242314c
guptadeepak155 <147677010+guptadeepak155@users.noreply.github.com>
2023-10-13 21:26:21 +0530

    Update python project.py

--- python project.py
+++ python project.py
@@ -1,15 +1,15 @@
 Height=float(input("Enter your height in centimeter:"))
-Weight=float(input("Enter your Weight in Kilogram:"))
+Weight=float(input("Enter your Weight in Kilogram(KG):"))
 Height=Height/100
 BMI=Weight/(Height*Height)
-print("your Body Mass Index(BMI) is:%d",BMI)
+print("your Body Mass Index (BMI) is:",BMI)
 if(BMI>0):
 	if(BMI<=16):
 		print("\nyou are severely underweight")
 		print("you must gain your wieght to look fit")
 	elif(BMI<=18.5):
 		print("\nyou are underweight")
-		print("we suggest you to gain your weight ")
+		print("we suggest you  to gain your weight ")
 	elif(BMI<=25):
 		print(" \ncongratulations!! you are Healthy")
 	elif(BMI<=30):
